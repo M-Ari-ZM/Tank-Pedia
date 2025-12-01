dbda0c0a0395bfb3e3136ccf355ebff1cc2e2aff
M-Ari-ZM <azm8216@gmail.com>
2025-12-01 11:22:52 +0700

    Add ani in card

--- src/js/home.js
+++ src/js/home.js
@@ -25,6 +25,19 @@ document.getElementById("btnHero").addEventListener("click", function () {
   });
 });
 
+// Animate Article
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const cardAni = document.querySelectorAll(".card");
+cardAni.forEach((el) => observer.observe(el));
+
 // Scroll Article
 const productContainer = [...document.querySelectorAll(".list-article")];
 const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
